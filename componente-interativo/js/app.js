f284d7e7b27dd988cc3e0e8f04b7eb0632462a2f
Eduardo <duardohenriqe19@gmail.com>
2023-07-02 12:47:38 -0300

    circles feedback selecionado

---
+++ componente-interativo/js/app.js
@@ -0,0 +1,17 @@
+function initSelected() {
+  const circles = document.querySelectorAll(".circles");
+  if (circles.length) {
+    function activeSelected() {
+      circles.forEach((circle) => {
+        circle.classList.remove("selected");
+      });
+      this.classList.toggle("selected");
+    }
+
+    circles.forEach((circle) => {
+      circle.addEventListener("click", activeSelected);
+    });
+  }
+}
+
+initSelected();
