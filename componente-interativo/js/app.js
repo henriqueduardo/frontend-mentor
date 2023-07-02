1cd5160c42371eb7a77c5a6334b8d21209bafe1b
Eduardo <duardohenriqe19@gmail.com>
2023-07-02 14:32:15 -0300

    exibir opção selecionada

--- componente-interativo/js/app.js
+++ componente-interativo/js/app.js
@@ -6,6 +6,11 @@ function initSelected() {
         circle.classList.remove("selected");
       });
       this.classList.toggle("selected");
+
+      const selectedOption = this.querySelector("span").textContent;
+      feedback.querySelector(
+        ".feedback-selected"
+      ).textContent = `You selected ${selectedOption} out of 5`;
     }
 
     circles.forEach((circle) => {
@@ -21,7 +26,9 @@ const btn = document.querySelector(".btn");
 
 function activeFeedbackMessage() {
   feedback.classList.add("active");
+  setTimeout(() => {
+    feedback.classList.remove("active");
+  }, 5000);
 }
+
 btn.addEventListener("click", activeFeedbackMessage);
-
-
