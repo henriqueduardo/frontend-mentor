32ec96a6d32a1cc88713d367c5ec733b46c7dfd5
Eduardo <duardohenriqe19@gmail.com>
2023-07-03 15:16:30 -0300

    validar input email

---
+++ base-apparel-input/js/app.js
@@ -0,0 +1,23 @@
+function input(event) {
+  event.preventDefault();
+  let emailInput = document.querySelector("#email");
+  let email = emailInput.value;
+  let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  let errorEmail = document.getElementById("error-email");
+  let errorIcon = document.getElementById("error-icon")
+  let successEmail = document.getElementById("success-email");
+
+  if (!email || !email.match(emailPattern)) {
+    errorEmail.innerHTML = "Please provide a valid email!";
+    successEmail.innerHTML = "";
+    emailInput.classList.add("error");
+    errorIcon.classList.add("active");
+    return false;
+  } else {
+    errorEmail.innerHTML = "";
+    successEmail.innerHTML = "Success! Your submission has been sent.";
+    emailInput.classList.remove("error");
+    errorIcon.classList.remove("active");
+    return true;
+  }
+}
