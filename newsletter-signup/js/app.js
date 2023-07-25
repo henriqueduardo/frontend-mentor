7bf04217dd18817358c80d44a3c5183f02688d76
Eduardo Henrique <duardohenriqe19@gmail.com>
2023-07-25 19:54:15 -0300

    modal de sucesso

--- newsletter-signup/js/app.js
+++ newsletter-signup/js/app.js
@@ -1,7 +1,11 @@
 let email = document.querySelector("#input-email");
+const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 const error = document.querySelector(".error-span");
 const btn = document.querySelector("#btn");
-const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const form = document.querySelector(".form-content");
+const modal = document.querySelector(".modal");
+const btnDismiss = document.querySelector("#btn-dismiss");
+const emailSend = document.querySelector(".email-send");
 
 function validateForm(event) {
   event.preventDefault();
@@ -11,10 +15,18 @@ function validateForm(event) {
     email.classList.add("error");
     email.focus();
   } else {
+    emailSend.textContent = email.value;
     error.classList.remove("active");
     email.classList.remove("error");
-    alert("email validado");
+    form.classList.add("disabled");
+    modal.classList.add("active");
   }
 }
 
+function dismissMessage() {
+  modal.classList.remove("active");
+  form.classList.remove("disabled");
+}
+
 btn.addEventListener("click", validateForm);
+btnDismiss.addEventListener("click", dismissMessage);
