40872a33b30c888faa345fb576474cf0c36b85ee
Eduardo Henrique <duardohenriqe19@gmail.com>
2023-08-21 16:32:47 -0300

    custom percentage, countdown e checkeds off

--- tip-calculator/js/app.js
+++ tip-calculator/js/app.js
@@ -2,26 +2,47 @@ const bill = document.querySelector("[data-tip='bill']");
 const numPeople = document.querySelector("[data-tip='peoples']");
 const percentage = document.querySelectorAll("[data-tip='percentage']");
 const showTipAmount = document.querySelector("[data-tip='tip-amount']");
+const showTipTotal = document.querySelector("[data-tip='tip-total']");
 
 const form = document.querySelector("[data-tip='form']");
 const btn = document.querySelector("[data-tip='reset']");
 
-let selectedPercentage = 5; // iniciar em 5 (checked)
+let selectedPercentage;
+let customPercentage = document.getElementById("customPercentage");
 
 // pegar valor da % selecionada
 percentage.forEach((i) => {
   i.addEventListener("click", () => {
     selectedPercentage = parseFloat(i.value);
+    console.log(selectedPercentage);
     calculateTip(); // executar a função de calculo com % selecionada
+    removeCheckedCustom(); // remover o efeito de checked
   });
 });
+
+// desmarcar inputs radio percentage
+customPercentage.addEventListener("click", () => {
+  percentage.forEach((radio) => {
+    radio.checked = false;
+  });
+});
+
+function checkedCustom() {
+  customPercentage.classList.add("checked-effect");
+}
+
+function removeCheckedCustom() {
+  customPercentage.classList.remove("checked-effect");
+}
+
+customPercentage.addEventListener("click", checkedCustom);
 
 function calculateTip() {
 
   let billValue = parseFloat(bill.value);
   let numPeopleValue = parseFloat(numPeople.value);
 
-  // verificar se é um nan ou 0
+  // verificar se é nan ou 0
   if (isNaN(numPeopleValue) || numPeopleValue === 0) {
     numPeopleValue = 1; // mockado p nao exibir nan
   }
@@ -29,18 +50,41 @@ function calculateTip() {
     billValue = 1;
   }
 
-  const tipAmount = (billValue * (selectedPercentage / 100)) / numPeopleValue;
-  showTipAmount.textContent = `$${tipAmount.toFixed(2)}`;
+  let tipAmount = 0;
+  let total = 0;
+
+  // verificar se uma porcentagem foi selecionada
+  if (!isNaN(selectedPercentage) && selectedPercentage !== 0) {
+    tipAmount = (billValue * (selectedPercentage / 100)) / numPeopleValue;
+    total = (billValue / numPeopleValue + tipAmount);
+  }
+
+  // iniciar em 0 e ir ate amount e total
+  countdown(0, tipAmount, showTipAmount);
+  countdown(0, total, showTipTotal);
+}
+
+function countdown(start, end, targetElement) {
+  let current = start;
+  const interval = setInterval(() => {
+    targetElement.textContent = `$${current.toFixed(2)}`;
+    current += 0.01; // ir de 1 em 1
+
+    if (current >= end) {
+      clearInterval(interval); // parar no resultado
+    }
+  }, 1); // tempo
 }
 
 function validateData() {
 
   const bill = document.getElementById("bill");
-  const peoples = document.getElementById("peoples")
+  const peoples = document.getElementById("peoples");
 
   if (bill.value === "") {
     bill.classList.add("active");
     bill.nextElementSibling.classList.add("active");
+    bill.focus();
   } else {
     bill.classList.remove("active");
     bill.nextElementSibling.classList.remove("active");
@@ -49,18 +93,18 @@ function validateData() {
   if (peoples.value === "") {
     peoples.classList.add("active");
     peoples.nextElementSibling.classList.add("active");
+    peoples.focus();
   } else {
     peoples.classList.remove("active");
     peoples.nextElementSibling.classList.remove("active");
   }
 
-  form.addEventListener("keyup", calculateTip);
+  form.addEventListener("change", calculateTip);
 }
 
-function buttonText() {
+function resetTips() {
   showTipAmount.textContent = "$0.00";
 }
 
-btn.addEventListener("click", buttonText);
-form.addEventListener('keyup', validateData);
-
+btn.addEventListener("click", resetTips);
+form.addEventListener('change', validateData);
